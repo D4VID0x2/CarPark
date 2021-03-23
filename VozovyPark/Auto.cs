dabbf93007d9dc8faa41c8988d294610aad9eb0f
D4VIDB2 <blaha.david.2018@gmail.com>
2021-03-23 16:20:39 +0100

    auto model znacka string

--- VozovyPark/Auto.cs
+++ VozovyPark/Auto.cs
@@ -6,12 +6,12 @@ namespace VozovyPark
     public class Auto
     {
         public int Uid { get; private set; }
-        public ZnackaAuta Znacka { get; private set; }
-        public ModelAuta Model { get; private set; }
+        public string Znacka { get; private set; }
+        public string Model { get; private set; }
         public TypAuta Typ { get; private set; }
         public double SpotrebaNa100km { get; private set; }
 
-        public Auto(ZnackaAuta znacka, ModelAuta model, TypAuta typ, double spotreba)
+        public Auto(string znacka, string model, TypAuta typ, double spotreba)
         {
             Uid = UID.newUID<Auto>();
             this.Znacka = znacka;
@@ -21,16 +21,6 @@ namespace VozovyPark
         }
 
     }
-    public enum ZnackaAuta
-    {
-        Skoda,
-    }
-
-    public enum ModelAuta
-    {
-        Octavia,
-    }
-
 
     public enum TypAuta
     {
